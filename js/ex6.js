b257b8d9aad647b51198e734f184402abfcbd6d6
jeffritoo <jeffryvdavid@gmail.com>
2021-03-02 12:36:10 -0800

    12:36PM

--- js/ex6.js
+++ js/ex6.js
@@ -1,5 +1,5 @@
 console.log("Jeffry's Output from Tutorial 4 Example 6");
-
+/*
 const cobElement = document.createElement("li");
 
 cobElement.id = "cob "
@@ -7,3 +7,18 @@ cobElement.id = "cob "
 cobElement.textContent = "<a href = https://www.csulb.edu/college-of-business> College of Business </a>"
 
 document.getElementById("links").insertBefore(cobElement,document.getElementById("is"));
+*/
+
+var a = document.createElement('a');
+
+var linkText = document.createTextNode('College of Business');
+
+a.appendChild(linkText);
+
+a.title = "College of Business";
+
+a.href = "https://www.csulb.edu/college-of-business";
+
+
+document.getElementById("links").insertBefore(a,document.getElementById("is"));
+
---
+++ js/ex7.js
@@ -0,0 +1,18 @@
+console.log("Jeffry's Output from Tutorial 4 Example 6");
+
+
+
+
+
+const words = [{
+    term: "Procrastination",
+    definition: "Avoidance of doing a task that needs to be accomplished"
+  }, {
+    term: "Tautology",
+    definition: "logical argument constructed in such a way that it is logically irrefutable"
+  }, {
+    term: "Oxymoron",
+    definition: "figure of speech that juxtaposes elements that appear to be contradictory"
+  }];
+  
+
